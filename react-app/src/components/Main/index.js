fa05f4832118a241c491593f8f123e697f088c7b
Michael Jung <jung.michaelh@gmail.com>
2022-10-14 11:17:55 -0700

    Add bizs, reviews store shape, sitewide CSS, favicon, icons, Nav & Footer components

---
+++ react-app/src/components/Main/index.js
@@ -0,0 +1,11 @@
+export default function Main() {
+
+
+    return (
+        <main>
+            <h1>Testing the splash page</h1>
+            <section></section>
+            <section></section>
+        </main>
+    )
+}
