70dddfa703c2e20f1331b74ed6a220b4fa31af86
Michael Jung <jung.michaelh@gmail.com>
2022-10-14 15:21:39 -0700

    Review routes - update, delete, add img, delete img

--- app/__init__.py
+++ app/__init__.py
@@ -8,7 +8,8 @@ from flask_login import LoginManager
 from .models import db, User
 from .api.user_routes import user_routes
 from .api.auth_routes import auth_routes
-from  .api.reviews_routes import reviews_routes
+from .api.reviews_routes import reviews_routes
+from .api.images_routes import images_routes
 
 from .seeds import seed_commands
 
@@ -33,6 +34,7 @@ app.config.from_object(Config)
 app.register_blueprint(user_routes, url_prefix='/api/users')
 app.register_blueprint(auth_routes, url_prefix='/api/auth')
 app.register_blueprint(reviews_routes, url_prefix='/api/reviews')
+app.register_blueprint(images_routes, url_prefix='/api/images')
 db.init_app(app)
 Migrate(app, db)
 
