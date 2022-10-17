23b641672be6055908b75d047a7c46f6219b0f97
Jae Hwang <jaeyoung1997@gmail.com>
2022-10-16 22:07:28 -0700

    get current user businesses route implemented

--- app/api/business_routes.py
+++ app/api/business_routes.py
@@ -17,12 +17,14 @@ def get_all_businesses():
     businesses = Business.query.all()
     biz = [business.to_dict() for business in businesses]
     for b in biz:
-        query = db.session.query(func.round(func.avg(Review.rating) * 2)/2).filter_by(business_id=b['id']).first()
+        query = db.session.query(func.round(
+            func.avg(Review.rating) * 2)/2).filter_by(business_id=b['id']).first()
         avg_rating = list(query)[0]
         b['avg_rating'] = avg_rating
     return jsonify({
         "Businesses": biz
-        })
+    })
+
 
 @business_routes.route("/<int:biz_id>/reviews", methods=['POST'])
 def add_review(biz_id):
@@ -44,6 +46,7 @@ def add_review(biz_id):
     # return {'errors': validation_errors_to_error_messages(form.errors)}, 401
     return "Bad data"
 
+
 @business_routes.route("/<int:biz_id>/")
 def get_one_business(biz_id):
     """
@@ -61,7 +64,38 @@ def get_one_business(biz_id):
     business['avg_rating'] = avg_rating
     business['Business_Images'] = images
     business['Owner'] = owner
-    
+
     return jsonify({
         "Businesses": business
     })
+
+
+@business_routes.route("/current/")
+def get_current_user_business():
+    """
+    Gets current user's business' details
+    """
+    user = current_user.to_dict()
+    user_id = user['id']
+    # print("CURRENT USER", user['id'])
+    businesses = Business.query.filter_by(owner_id=user_id)
+    all_businesses = [b.to_dict() for b in businesses]
+    print("ALL BUSINESSES", all_businesses)
+    current_businesses = []
+    if len(all_businesses) > 0:
+        for business in all_businesses:
+            query = db.session.query(func.round(
+            func.avg(Review.rating) * 2)/2).filter_by(business_id=business['id']).first()
+            avg_rating = list(query)[0]
+            business_images = Image.query.filter_by(business_id=business['id'])
+            images = [{"id": img.to_dict()['id'], "url": img.to_dict()['url'], "review_id": img.to_dict()['review_id']}
+                  for img in business_images]
+
+            business['avg_rating'] = avg_rating
+            business['Business_Images'] = images
+            current_businesses.append(business)
+
+        return jsonify({
+            "Businesses": current_businesses
+        })
+    return 'Current user does not have any listed businesses'
