19187866136bcd27e7b5e05e197c1391cdeddec9
Amanda Vien <amandakvien@gmail.com>
2022-10-19 10:18:43 -0700

    added toggle styling for type/transactions buttons

--- react-app/src/components/Biz/index.js
+++ react-app/src/components/Biz/index.js
@@ -14,7 +14,15 @@ export default function Biz() {
     const bizArr = Object.values(biz);
     const dispatch = useDispatch();
     const [pageNum, setPageNum] = useState(0);
+    // const [active, setActive] = useState(false);
+    const [activePrice, setActivePrice] = useState(false);
+    const [activeOpen, setActiveOpen] = useState(false);
+    const [activeDel, setActiveDel] = useState(false);
+    const [activeTakeout, setActiveTakeout] = useState(false);
+    const [activeRes, setActiveRes] = useState(false);
 
+
+    /* ----------PAGINATION FXNS/LOGIC---------- */
     const bizPerPage = 10;
     const visitedPages = pageNum * bizPerPage;
     const pageCount = Math.ceil(bizArr.length / bizPerPage);
@@ -25,6 +33,8 @@ export default function Biz() {
     const bizzies = bizArr.slice(visitedPages, visitedPages + bizPerPage)
     // console.log('this the bizzies', bizzies)
 
+
+    /* -------------TIME FXNS/LOGIC------------- */
     let current_time;
     const settingTime = () => {
         const today = new Date();
@@ -47,23 +57,71 @@ export default function Biz() {
     }
 
 
+    /* ------------TOGGLE FXNS/LOGIC------------ */
+    const toggleIdPrice = () => {
+        setActivePrice(!activePrice);
+    }
+    const toggleIdOpen = () => {
+        setActiveOpen(!activeOpen);
+    }
+    const toggleIdDel = () => {
+        setActiveDel(!activeDel);
+    }
+    const toggleIdTakeout = () => {
+        setActiveTakeout(!activeTakeout);
+    }
+    const toggleIdRes = () => {
+        setActiveRes(!activeRes);
+    }
+
+    
+    /* ---------------USE EFFECT--------------- */
     useEffect(() => {
         dispatch(getAllBiz())
     }, [dispatch])
+
 
     return (
         <main className="main">
             <h1 className="allbiz-title">Best Food Near Me in City, State</h1>
             <div className="types-buttons">
-                <button id='type-butt'>
+                <button
+                    id={activePrice ? 'type-butt-price-act' : 'type-butt'}
+                    onClick={toggleIdPrice}
+                >
                     Price
                     <svg width='12' height='12'><path d="M 8 10.25 a 0.746 0.746 0 0 1 -0.525 -0.215 l -3.055 -3 a 0.75 0.75 0 0 1 1.05 -1.07 L 8 8.449 l 2.53 -2.484 a 0.75 0.75 0 0 1 1.05 1.07 l -3.055 3 A 0.746 0.746 0 0 1 8 10.25 Z"></path></svg>
                 </button>
-                <button id='type-butt'>Open Now</button>
-                <button id='type-butt'>Offers Delivery</button>
-                <button id='type-butt'>Offers Takeout</button>
-                <button id='type-butt'>Reservations</button>
-                <button id='type-butt'>Breakfast & Brunch</button>
+                <button
+                    id={activeOpen ? 'type-butt-act' : 'type-butt'}
+                    onClick={toggleIdOpen}
+                >
+                    Open Now
+                </button>
+                <button
+                    id={activeDel ? 'type-butt-act' : 'type-butt'}
+                    onClick={toggleIdDel}
+                >
+                    Offers Delivery
+                </button>
+                <button
+                    id={activeTakeout ? 'type-butt-act' : 'type-butt'}
+                    onClick={toggleIdTakeout}
+                >
+                    Offers Takeout
+                </button>
+                <button
+                    id={activeRes ? 'type-butt-act' : 'type-butt'}
+                    onClick={toggleIdRes}
+                >
+                    Reservations
+                </button>
+                {/* <button
+                    id={active ? 'type-butt-act-4' : 'type-butt'}
+                    onClick={toggleId}
+                >
+                    Breakfast & Brunch
+                </button> */}
             </div>
             <ol>
                 {bizzies.map(biz => (
