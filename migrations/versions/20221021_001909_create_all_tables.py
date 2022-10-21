32f141db1b6c41a10e46d4b99c011b87cefb16bf
Amanda Vien <amandakvien@gmail.com>
2022-10-21 00:19:37 -0700

    reseeded data

--- migrations/versions/20221021_001909_create_all_tables.py
+++ migrations/versions/20221021_001909_create_all_tables.py
@@ -1,8 +1,8 @@
 """Create all tables
 
-Revision ID: 251f5329aed5
+Revision ID: 9298b4cdc1ff
 Revises: 
-Create Date: 2022-10-20 15:24:43.769192
+Create Date: 2022-10-21 00:19:09.882470
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '251f5329aed5'
+revision = '9298b4cdc1ff'
 down_revision = None
 branch_labels = None
 depends_on = None
