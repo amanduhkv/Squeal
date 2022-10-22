530fd4650d159071acfac7d6363e304b6c5dba20
Michael Jung <jung.michaelh@gmail.com>
2022-10-21 21:41:44 -0700

    Update stars logic for search when no reviews

--- migrations/versions/20221021_213731_create_all_tables.py
+++ migrations/versions/20221021_213731_create_all_tables.py
@@ -1,8 +1,8 @@
 """Create all tables
 
-Revision ID: 6f5dc34320b8
+Revision ID: f87bef907709
 Revises: 
-Create Date: 2022-10-21 13:47:38.596459
+Create Date: 2022-10-21 21:37:31.162183
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '6f5dc34320b8'
+revision = 'f87bef907709'
 down_revision = None
 branch_labels = None
 depends_on = None
