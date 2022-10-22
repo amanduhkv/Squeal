cc200c6e6fb1b83aca3f8ecf3022bb217c8cb2e6
Jae Hwang <jaeyoung1997@gmail.com>
2022-10-21 23:25:23 -0700

    seeder data

--- migrations/versions/20221021_213646_create_all_tables.py
+++ migrations/versions/20221021_213646_create_all_tables.py
@@ -1,8 +1,8 @@
 """Create all tables
 
-Revision ID: 6f5dc34320b8
+Revision ID: 32db8163ae5d
 Revises: 
-Create Date: 2022-10-21 13:47:38.596459
+Create Date: 2022-10-21 21:36:46.552076
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '6f5dc34320b8'
+revision = '32db8163ae5d'
 down_revision = None
 branch_labels = None
 depends_on = None
